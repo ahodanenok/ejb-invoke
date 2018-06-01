af5f2971c1c5306e1c3adc12e3c1273e7b63737e
Andrei Hodanenok <xoda91@gmail.com>
2018-06-02 00:47:43 +0300

    removed unused method

--- src/main/java/ahodanenok/ejb/invoke/util/ReflectionUtils.java
+++ src/main/java/ahodanenok/ejb/invoke/util/ReflectionUtils.java
@@ -14,38 +14,6 @@ public final class ReflectionUtils {
 
     private ReflectionUtils() { }
 
-    public List<Class> toClasses(List<String> classNames) {
-        try {
-            List<Class> classes = new ArrayList<Class>(classNames.size());
-
-            String argClass;
-            for (int i = 0; i < classNames.size(); i++) {
-                argClass = classNames.get(i);
-                if ("double".equals(argClass)) {
-                    classes.add(double.class);
-                } else if ("float".equals(argClass)) {
-                    classes.add(float.class);
-                } else if ("byte".equals(argClass)) {
-                    classes.add(byte.class);
-                } else if ("short".equals(argClass)) {
-                    classes.add(short.class);
-                } else if ("int".equals(argClass)) {
-                    classes.add(int.class);
-                } else if ("long".equals(argClass)) {
-                    classes.add(long.class);
-                } else if ("boolean".equals(argClass)) {
-                    classes.add(boolean.class);
-                } else {
-                    classes.add(Class.forName(argClass, true, Thread.currentThread().getContextClassLoader()));
-                }
-            }
-
-            return classes;
-        } catch (Exception e) {
-            throw new RuntimeException("Can't load classes", e);
-        }
-    }
-
     public static ClassLoader createClassLoader(List<String> paths, ClassLoader parent) {
         List<URL> urls = new ArrayList<URL>(paths.size());
         for (int i = 0; i < paths.size(); i++) {
